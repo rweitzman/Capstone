05cb5a4208c627d7f1f2395371453b8bc26f61a4
Robin Weitzman <rl_weitzman@yahoo.com>
2021-06-03 20:44:46 -0700

    needing to stop due to technical difficulties

--- models/Recipes.js
+++ models/Recipes.js
@@ -1,7 +1,7 @@
 const Recipes = [
         {name: "Beef and Potatoes", img: "/images/beefdogfood.jpeg", ingredients: "Beef, potaotes", description: "Beef and potatoes to keep your dog happy and fed"},
-        {name: "Chicken and Rice", img: "", ingredients: "Chicken, rice", description: "Chicken and rice to keep your dog happy and fed"},
-        {name: "Salmon and Squash", img: "", ingredients: "Salmon, squash", description: "Salmon and squash to keep your dog happy and fed"},
+        {name: "Chicken and Rice", img: "/images/chickendogfood.jpeg", ingredients: "Chicken, rice", description: "Chicken and rice to keep your dog happy and fed"},
+        {name: "Salmon and Squash", img: "/images/salmondogfood.jpeg", ingredients: "Salmon, squash", description: "Salmon and squash to keep your dog happy and fed"},
 ];
 
 module.exports = Recipes;
