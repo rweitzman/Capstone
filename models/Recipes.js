390e93a3f977d7fcf31833b21b7e7cb1ec7ad644
Robin Weitzman <rl_weitzman@yahoo.com>
2021-06-03 20:07:39 -0700

    worked on functionality

--- models/Recipes.js
+++ models/Recipes.js
@@ -1,6 +1,5 @@
 const Recipes = [
-    
-        {name: "Beef and Potatoes", img: "", ingredients: "Beef, potaotes", description: "Beef and potatoes to keep your dog happy and fed"},
+        {name: "Beef and Potatoes", img: "/images/beefdogfood.jpeg", ingredients: "Beef, potaotes", description: "Beef and potatoes to keep your dog happy and fed"},
         {name: "Chicken and Rice", img: "", ingredients: "Chicken, rice", description: "Chicken and rice to keep your dog happy and fed"},
         {name: "Salmon and Squash", img: "", ingredients: "Salmon, squash", description: "Salmon and squash to keep your dog happy and fed"},
 ];
